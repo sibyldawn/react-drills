3e3472008cc9f0ff5f5a1e6b8b698ad5bfa9a265
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-26 09:27:41 -0700

    "Add Image"

---
+++ app-5/src/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AC from './Ac.jpg';
+
+export default function Image( props ) {
+  return (
+    <div>
+      <img src={ props.myImage } alt="Assasination Classroom Image" />
+    </div>
+  )
+}
+  
+  
--- app-5/src/index.js
+++ app-5/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Image from './Image';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
