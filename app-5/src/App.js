38cb3fa39a82ebdfdb9069150255501a6f073a27
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-26 13:37:58 -0700

    "Add Todo Component"

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Image from './Image';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image myImage={ "https://vignette.wikia.nocookie.net/vsbattles/images/b/b9/Ac.jpg/revision/latest?cb=20160510013432" } />
+        <Image myImage={ "./Ac.jpg" } />
       </div>
     );
   }
--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      userInput:"",
+      tasks: []
+    }
+  }
+
+  handleChange(val){
+    this.setState({
+        userInput: val
+    })
+  }
+
+  addToList(task){
+    
+
+  }
+
   render() {
     return (
+      
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={this.handleChange(e.target.val)} type ="text">Enter new task</input>
+      <button onClick={this.addToList(e.target.val)}>Add</button>
       </div>
     );
   }
