1af19e857bf3c21bbb755f4217e0198703bdbe00
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-27 21:52:53 -0700

    Complete Set 1-5

---
+++ app-6/src/Todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Todo(props) {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
